4a5ff0f55066dad6750ac912f0e4c5084bf56542
Afaq Khan <afaqhussain.khan02@gmail.com>
2025-12-11 11:57:54 +0500

    Add files via upload

---
+++ database.py
@@ -0,0 +1,41 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import config
+
+DATABASE_URL = (
+    f"postgresql://{config.DB_USER}:{config.DB_PASS}@{config.DB_HOST}:{config.DB_PORT}/{config.DB_NAME}"
+)
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+Base = declarative_base()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+
+
+
+# # database.py → NAYA AUR BEST VERSION
+# from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+# from sqlalchemy.orm import declarative_base
+# import config
+
+# DATABASE_URL = f"postgresql+asyncpg://{config.DB_USER}:{config.DB_PASS}@{config.DB_HOST}:{config.DB_PORT}/{config.DB_NAME}"
+
+# engine = create_async_engine(DATABASE_URL, echo=False)
+# AsyncSessionLocal = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+# Base = declarative_base()
+
+# async def get_db():
+#     async with AsyncSessionLocal() as session:
+#         yield session
