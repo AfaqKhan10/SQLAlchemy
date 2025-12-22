cf2717971407edb0f2fab7a30b157d81039eb849
Afaq Khan <afaqhussain.khan02@gmail.com>
2025-12-22 11:39:17 +0500

    Update models.py

--- models.py
+++ models.py
@@ -4,7 +4,6 @@ from sqlalchemy import Column, Integer, String, ForeignKey ,Table , Boolean
 
 Base = declarative_base()
 
-#  User table model
 class User(Base):
     __tablename__ = "users"  e
     id = Column(Integer, primary_key=True)  
@@ -16,9 +15,6 @@ class User(Base):
 
     orders = relationship("Order", back_populates="user")  
     
-
-
-# Bich ka table — ye batata hai kis order mein kon sa product hai
 order_product = Table(
     "order_product",
     Base.metadata,
@@ -26,26 +22,19 @@ order_product = Table(
     Column("product_id", Integer, ForeignKey("products.id"), primary_key=True)
 )
 
-# Order table model
+
 class Order(Base):
     __tablename__ = "orders"
-    id = Column(Integer, primary_key=True)  # Order ka unique ID
-    item = Column(String, nullable=False)  # Item name
-    user_id = Column(Integer, ForeignKey("users.id"))  # Foreign key to link User
-    user = relationship("User", back_populates="orders")  # Link back to User
+    id = Column(Integer, primary_key=True) 
+    item = Column(String, nullable=False)  
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user = relationship("User", back_populates="orders")  
     products = relationship("Product", secondary=order_product, back_populates="orders")
 
 
-
-# Product table (jaise "Biryani", "Coke", "Pizza")
 class Product(Base):
     __tablename__ = "products"
     id = Column(Integer, primary_key=True)
     name = Column(String(100), nullable=False, unique=True)
-
-    # Is product kitne orders mein hai
+    
     orders = relationship("Order", secondary=order_product, back_populates="products")
-
-
-
-
